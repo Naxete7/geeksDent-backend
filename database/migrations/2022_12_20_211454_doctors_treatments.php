f681d3f141033cb4621addd1fb8c02311e108035
Ignacio <naxete7@hotmail.es>
2022-12-20 23:19:29 +0100

    feat:migrations finished

---
+++ database/migrations/2022_12_20_211454_doctors_treatments.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('treatments', function (Blueprint $table) {
+            $table->id();
+           
+            $table->unsignedBigInteger('doctorsId');
+            $table->unsignedBigInteger('treatmentsId');
+
+            $table->foreign('doctorsId')->references('id')->on('doctors');
+            $table->foreign('treatmentsId')->references('id')->on('treatments');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('doctors_treatments');
+    }
+};
